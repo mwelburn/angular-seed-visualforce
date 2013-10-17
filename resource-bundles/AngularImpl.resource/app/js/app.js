5af011be97d435254b81648de0856b24e9d73802
Michael Welburn <mwelbu2@gmail.com>
2013-10-16 22:21:20 -0400

    Initial version of angular-seed

---
+++ resource-bundles/AngularImpl.resource/app/js/app.js
@@ -0,0 +1,16 @@
+'use strict';
+
+
+// Declare app level module which depends on filters, and services
+angular.module('angularDemo', [
+  'ngRoute',
+  'angularDemo.filters',
+  'angularDemo.services',
+  'angularDemo.directives',
+  'angularDemo.controllers'
+]).
+config(['$routeProvider', function($routeProvider) {
+  $routeProvider.when('/view1', {templateUrl: '/apex/partial1', controller: 'MyCtrl1'});
+  $routeProvider.when('/view2', {templateUrl: '/apex/partial2', controller: 'MyCtrl2'});
+  $routeProvider.otherwise({redirectTo: '/view1'});
+}]);
---
+++ resource-bundles/AngularImpl.resource/app/js/controllers.js
@@ -0,0 +1,11 @@
+'use strict';
+
+/* Controllers */
+
+angular.module('angularDemo.controllers', []).
+  controller('MyCtrl1', [function() {
+
+  }])
+  .controller('MyCtrl2', [function() {
+
+  }]);
